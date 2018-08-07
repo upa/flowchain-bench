41712b8e6851244db7134e6e7110cfe877ae9172
Ryo Nakamura <upa@haeena.net>
2018-08-07 19:49:57 +0900

    update graphs

--- all.sh
+++ all.sh
@@ -2,13 +2,13 @@
 
 ./parser-flow-num-vs-install-time.py \
 	output/flow-num-vs-install_per-10flows_2.txt \
+	output/flow-num-vs-install_per-10flows_over980.txt \
 	> dat/flow-num-vs-install.dat
-
 
 ./parser-chain-num-vs-delay.py \
 	output/chain-num-vs-deley.txt \
 	> dat/chain-num-vs-delay.dat
 
 ./parser-exabgp-update-time.py \
-	output/exabgp-update-time_2.txt \
+	output/exabgp-update-time_3.txt \
 	> dat/exabgp-update-time.dat
