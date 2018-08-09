f367fa2982b578c86538175e1869c28397f2899d
Ryo Nakamura <upa@haeena.net>
2018-08-09 17:54:21 +0900

    exabgp retry

--- test-exabgp-update-time.py
+++ test-exabgp-update-time.py
@@ -3,20 +3,25 @@
 
 import time
 import math
+import socket
 from subprocess import Popen, PIPE
 
 from optparse import OptionParser
 
 
-def send_fc_cmd(pipe, cmd) :
+ctl_socket = None
+
+def send_fc_cmd(cmd) :
+
+    global ctl_socket
 
     print(cmd)
-    pipe.write(cmd.encode())
-    time.sleep(0.001)
+    ctl_socket.send(cmd.encode())
+    ret = ctl_socket.recv(129)
 
 
 
-def install_numbered_flow(pipe, flownum) :
+def install_numbered_flow(flownum) :
 
     o3 = math.floor(flownum / 255)
     o4 = flownum % 255
@@ -26,37 +31,37 @@ def install_numbered_flow(pipe, flownum) :
     fc_cmd = "/add/%s/none/none/user-global/fp1-fn1" % prefix
     ctl_cmd = "GET %s\n" % fc_cmd
 
-    send_fc_cmd(pipe, ctl_cmd)
+    send_fc_cmd(ctl_cmd)
 
     return
 
-def install_bulk_flow(pipe, num) :
+def install_bulk_flow(num) :
     ctl_cmd = "BULK %d" % num
-    send_fc_cmd(pipe, ctl_cmd)
+    send_fc_cmd(ctl_cmd)
     return
 
-def install_msmt_flow(pipe) :
+def install_msmt_flow() :
     prefix = "45.0.80.1/32"
     fc_cmd = "/add/%s/none/none/user-global/fp1-fn1" % prefix
     ctl_cmd = "GET %s\n" % fc_cmd
-    send_fc_cmd(pipe, ctl_cmd)
+    send_fc_cmd(ctl_cmd)
 
     return
 
-def uninstall_msmt_flow(pipe) :
+def uninstall_msmt_flow() :
     prefix = "45.0.80.1/32"
     fc_cmd = "/delete/%s" % prefix
     ctl_cmd = "GET %s\n" % fc_cmd
 
-    send_fc_cmd(pipe, ctl_cmd)
+    send_fc_cmd(ctl_cmd)
 
     return
 
 
-def destroy_flow(pipe) :
+def destroy_flow() :
     fc_cmd = "/destroy"
     ctl_cmd = "GET %s\n" % fc_cmd
-    send_fc_cmd(pipe, ctl_cmd)
+    send_fc_cmd(ctl_cmd)
 
     return
 
@@ -65,22 +70,24 @@ def destroy_flow(pipe) :
 def main() :
 
     # open unix domain socket for controlling fc process
-    p = Popen(["nc", "-uU", "/tmp/fc.sock"], bufsize = 0, stdin = PIPE)
+    global ctl_socket
+    ctl_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    ctl_socket.connect("/tmp/fc.sock")
     
-    for n in range(1, 180, 10) :
+    for n in range(1, 1000, 50) :
         
         if n == 1 :
             x = 1
         else :
             x = n - 1
 
-        send_fc_cmd(p.stdin, "ECHO Install BULK %d Flows\n" % x)
-        install_bulk_flow(p.stdin, x)
-        time.sleep(8)
+        send_fc_cmd("ECHO Install BULK %d Flows\n" % x)
+        install_bulk_flow(x)
+        time.sleep(10)
 
-        send_fc_cmd(p.stdin, "ECHO Destroy BULK %d Flows\n" % x)
-        destroy_flow(p.stdin)
-        time.sleep(8)
+        send_fc_cmd("ECHO Destroy BULK %d Flows\n" % x)
+        destroy_flow()
+        time.sleep(10)
 
     p.terminate()
 
